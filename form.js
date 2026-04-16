154f804c1cfb6df6a9d9253d2607af0e52d91c6f
Proshanta15 <roy15-6997@diu.edu.bd>
2026-04-16 09:01:20 +0600

    node project

--- form.js
+++ form.js
@@ -1,5 +1,7 @@
 const http = require("http");
 const fs = require("fs");
+const queryString = require("querystring");
+const { log } = require("console");
 
 // const server = http
 //   .createServer((req, res) => {
@@ -28,11 +30,46 @@ const server = http
         res.end();
         return;
       }
+
       if (req.url === "/") {
         res.writeHead(200, { "content-type": "text/html" });
         res.write(data);
         res.end();
       } else if (req.url === "/submit") {
+        let dataBody = [];
+        req.on("data", (chunk) => {
+          dataBody.push(chunk);
+        });
+
+        req.on("end", () => {
+          const parseData = Buffer.concat(dataBody).toString();
+          let redableData = queryString.parse(parseData);
+          console.log(redableData);
+          let dataString =
+            "My name is " +
+            redableData.name +
+            " and my email is " +
+            redableData.email;
+
+          // Sync method to write data to file
+          // fs.writeFileSync("text/" + redableData.name + ".txt", dataString);
+          // console.log("Data saved");
+
+          // Async method to write data to file
+          fs.writeFile(
+            "text/" + redableData.name + ".txt",
+            dataString,
+            "utf-8",
+            (err) => {
+              if (err) {
+                console.error("Error writing to file:", err);
+              } else {
+                console.log("Data saved in Async file");
+              }
+            },
+          );
+        });
+
         res.writeHead(200, { "content-type": "text/html" });
         res.write("<h1>Form Submitted</h1>");
         res.end();
